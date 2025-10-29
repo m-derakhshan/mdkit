ed8b56eb6b276feb46472e311af982fd6e6a563f
Mammad Derakhshan <mohammad.derakhshan@hiway.media>
2025-10-29 10:22:08 +0100

    clean up qr code scanner module

--- qr_scanner/build.gradle.kts
+++ qr_scanner/build.gradle.kts
@@ -1,66 +1,19 @@
+apply {
+    from("$rootDir/compose-base-module.gradle")
+}
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.dagger.hilt.android.plugin)
     alias(libs.plugins.google.devtools.ksp)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.jetbrains.kotlin.serialization.plugin)
 }
 
 android {
-    namespace = "media.hiway.mdkit.qr_scanner"
-    compileSdk {
-        version = release(36)
-    }
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-
-
-    buildFeatures {
-        compose = true
-    }
-
+    namespace = QRScanner.NAMESPACE
 }
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-}
-
 
 //-----------------camera x------------------------//
 dependencies {
@@ -71,11 +24,4 @@ dependencies {
     implementation(libs.androidx.camera.compose)
     implementation(libs.google.mlkit.barcode)
     implementation(libs.androidx.camera.camera2)
-}
-
-
-dependencies{
-    implementation(libs.google.dagger.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
-    ksp(libs.google.dagger.hilt.android.compiler)
 }
