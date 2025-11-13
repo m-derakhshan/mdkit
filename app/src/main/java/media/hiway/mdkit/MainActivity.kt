36f45f1256f1c5e3c11291da381318af3a7ca650
Mammad Derakhshan <mohammad.derakhshan@hiway.media>
2025-11-13 11:04:26 +0100

    adding all examples to main

--- app/src/main/java/media/hiway/mdkit/MainActivity.kt
+++ app/src/main/java/media/hiway/mdkit/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import dagger.hilt.android.AndroidEntryPoint
+import media.hiway.mdkit.examples.floating_view.FloatingViewExample
+import media.hiway.mdkit.examples.permission.PermissionExample
 import media.hiway.mdkit.examples.qr_scanner.QRScannerExample
+import media.hiway.mdkit.examples.translator.TranslatorExample
 import media.hiway.mdkit.translator.domain.use_case.Translator
 import media.hiway.mdkit.ui.theme.MDKitTheme
 import javax.inject.Inject
@@ -25,9 +28,9 @@ class MainActivity : ComponentActivity() {
             MDKitTheme {
 
                 //!------------Use each example at once. comment out the others-----------//
-//                TranslatorExample(translator = translator)
-//                FloatingViewExample()
-//                PermissionExample()
+                TranslatorExample(translator = translator)
+                FloatingViewExample()
+                PermissionExample()
                 QRScannerExample()
                 //!------------Use each example at once. comment out the others-----------//
 
