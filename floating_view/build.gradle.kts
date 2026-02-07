c807a0787fd5e51ea144068350087c8c542cfc4c
m-derakhshan <43886409+m-derakhshan@users.noreply.github.com>
2026-02-07 13:48:45 +0100

    prepare maven publish

--- floating_view/build.gradle.kts
+++ floating_view/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization.plugin)
     alias(libs.plugins.google.devtools.ksp)
+    id("maven-publish")
 }
 
 configure<LibraryExtension> {
--- qr_scanner/build.gradle.kts
+++ qr_scanner/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.serialization.plugin)
     alias(libs.plugins.google.devtools.ksp)
+    id("maven-publish")
 }
 
 
